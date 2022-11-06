820669a72dc0070f8daaacee5b3ab198c9b4e80a
Remigiusz Marszolik <wrx82001@student.wsb.wroclaw.pl>
2022-11-06 21:03:57 +0100

    v8

--- Statystyka.py
+++ Statystyka.py
@@ -1,23 +1,37 @@
-def suma(x):
-    return print("\nSuma listy to : ", sum(x))
+# def suma(x):
+#     return print("\nSuma listy to : ", sum(x))
+#
+#
+# def srednia(x):
+#     a = sum(x)
+#     b = len(x)
+#     c = (a / b)
+#     return print("\nWartość srednia listy to : ", str(c))
+#
+#
+# def min(x):
+#     x.sort()
+#     return print("\nWartość minimalna listy to : ", x[0])
+#
+#
+# def max(x):
+#     a = len(x) - 1
+#     return print("\nWartość maksymalna listy to : ", x[a])
+#
+#
+# lista = []
+# ilosc = eval(input("Ile chcesz wprowadzic liczb do listy?: "))
+# for x in range(ilosc):
+#     liczba = eval(input("Wprowardz liczbę do listy: "))
+#     lista.append(liczba)
+#
+# suma(lista)
+# srednia(lista)
+# min(lista)
+# max(lista)
 
-
-def srednia(x):
-    a = sum(x)
-    b = len(x)
-    c = (a / b)
-    return print("\nWartość srednia listy to : ", str(c))
-
-
-def min(x):
-    x.sort()
-    return print("\nWartość minimalna listy to : ", x[0])
-
-
-def max(x):
-    a = len(x) - 1
-    return print("\nWartość maksymalna listy to : ", x[a])
-
+import stats_local as sty
+import stats_global as stt
 
 lista = []
 ilosc = eval(input("Ile chcesz wprowadzic liczb do listy?: "))
@@ -25,7 +39,7 @@ for x in range(ilosc):
     liczba = eval(input("Wprowardz liczbę do listy: "))
     lista.append(liczba)
 
-suma(lista)
-srednia(lista)
-min(lista)
-max(lista)
+sty.suma(lista)
+sty.srednia(lista)
+stt.min(lista)
+stt.max(lista)
